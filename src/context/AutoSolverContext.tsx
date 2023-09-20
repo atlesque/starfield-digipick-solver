ef85d83a87a4950b50523e194c373d462c186acf
Andrew Hartwig <andrew.hartwig1@gmail.com>
2023-09-20 09:19:28 -0400

    fix: clean up the auto solver context for easier expansion

--- src/context/AutoSolverContext.tsx
+++ src/context/AutoSolverContext.tsx
@@ -1,104 +1,130 @@
-import { Dispatch, PropsWithChildren, SetStateAction, createContext, useCallback, useEffect, useMemo, useState } from 'react';
+import { Dispatch, MutableRefObject, PropsWithChildren, SetStateAction, createContext, useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 import { Difficulty, TOTAL_KEYS_BY_DIFFICULTY, TOTAL_LAYERS_BY_DIFFICULTY } from '../constants';
 import { DigiKey } from '../types/DigiKey';
 import { Puzzle } from '../types/Puzzle';
 import { solvePuzzle } from '../utils/solvePuzzle';
 import { NotSolveableError } from '../errors/NotSolveableError';
 
+/**
+ * Bump this version if breaking changes are made to state.
+ * It will reset what is stored inside of localStorage.
+ */
+const SAVED_STATE_VERSION = 1;
+
 type gapIllustrationMode = 'visual' | 'numbers' | 'none';
 interface AutoSolverContextValue {
   difficulty: Difficulty
-  setDifficulty: Dispatch<SetStateAction<Difficulty>>
   keys: DigiKey[]
-  setKeys: Dispatch<SetStateAction<DigiKey[]>>
   puzzle: Puzzle
-  setPuzzle: Dispatch<SetStateAction<Puzzle>>
   editKey: number
-  setEditKey: Dispatch<SetStateAction<number>>
-  onReset: () => void
-  onSolve: () => void
   solved: boolean
   error?: string
   activeLayer: string
-  setActiveLayer: Dispatch<SetStateAction<string>>
   guides: boolean
-  setGuides: Dispatch<SetStateAction<boolean>>
   mobileOffset: boolean
-  setMobileOffset: Dispatch<SetStateAction<boolean>>
   gapIllustrationMode: gapIllustrationMode
+}
+
+interface AutoSolverContextActions {
+  setDifficulty: Dispatch<SetStateAction<Difficulty>>
+  setKeys: Dispatch<SetStateAction<DigiKey[]>>
+  setPuzzle: Dispatch<SetStateAction<Puzzle>>
+  setEditKey: Dispatch<SetStateAction<number>>
+  setActiveLayer: Dispatch<SetStateAction<string>>
+  setGuides: Dispatch<SetStateAction<boolean>>
+  setMobileOffset: Dispatch<SetStateAction<boolean>>
   setGapIllustrationMode: Dispatch<SetStateAction<gapIllustrationMode>>
 }
 
-export const AutoSolverContext = createContext<AutoSolverContextValue>(null!);
+interface AutoSolverContextExtras {
+  state: AutoSolverContextValue
+  stateRef: MutableRefObject<AutoSolverContextValue>
+  onReset: () => void
+  onSolve: () => void
+}
+
+const defaultAutoSolverState: AutoSolverContextValue = {
+  difficulty: Difficulty.Novice,
+  keys: [],
+  solved: false,
+  error: undefined,
+  puzzle: { layers: [] },
+  editKey: -1,
+  activeLayer: '',
+  guides: false,
+  mobileOffset: false,
+  gapIllustrationMode: 'numbers'
+}
+
+type autoSolverContext = AutoSolverContextValue & AutoSolverContextActions & AutoSolverContextExtras;
+const reducer = (state: AutoSolverContextValue, action: Partial<AutoSolverContextValue>): AutoSolverContextValue => ({ ...state, ...action });
+const load = (init: AutoSolverContextValue): AutoSolverContextValue => {
+  try {
+    const savedState = JSON.parse(localStorage.getItem('save') ?? '')
+    if (savedState.version !== SAVED_STATE_VERSION) {
+      throw new Error('Version changed.');
+    }
+    delete savedState.version;
+    return {
+      init,
+      ...savedState
+    };
+  } catch (e) {
+    return init;
+  }
+}
+
+export const AutoSolverContext = createContext<autoSolverContext>(null!);
 
 export const AutoSolverProvider = ({ children }: PropsWithChildren) => {
-  const saved = useMemo(() => {
-    try {
-      return JSON.parse(localStorage.getItem('save') ?? '');
-    } catch (e) {
-      return {};
-    }
-  }, []);
-  const [difficulty, setDifficulty] = useState<Difficulty>(saved.difficulty ?? Difficulty.Novice);
-  const [keys, setKeys] = useState<DigiKey[]>(saved.keys ?? []);
-  const [solved, setSolved] = useState<boolean>(saved.solved ?? false);
-  const [error, setError] = useState<string>(saved.error ?? undefined);
-  const [puzzle, setPuzzle] = useState<Puzzle>(saved.puzzle ?? { layers: [] });
-  const [editKey, setEditKey] = useState<number>(saved.editKey ?? -1);
-  const [activeLayer, setActiveLayer] = useState(saved.activeLayer ?? '');
-  const [guides, setGuides] = useState(saved.guides ?? false);
-  const [mobileOffset, setMobileOffset] = useState(saved.guides ?? false);
-  const [gapIllustrationMode, setGapIllustrationMode] = useState(saved.gapIllustrationMode ?? 'numbers');
+  const [state, setState] = useReducer(reducer, defaultAutoSolverState, load);
+  const stateRef = useRef(state);
+  stateRef.current = state;
 
   useEffect(() => {
     localStorage.setItem('save', JSON.stringify({
-      difficulty,
-      keys,
-      solved,
-      error,
-      puzzle,
-      editKey,
-      activeLayer,
-      guides,
-      mobileOffset,
-      gapIllustrationMode
+      ...state,
+      version: SAVED_STATE_VERSION
     }))
-  }, [difficulty, keys, solved, error, puzzle, editKey, activeLayer, guides, mobileOffset, gapIllustrationMode]);
+  }, [state]);
 
   useEffect(() => {
-    setKeys(k => {
-      return Array.from<unknown, DigiKey>({ length: TOTAL_KEYS_BY_DIFFICULTY[difficulty] }, (_, i) => (
-        k[i] ?? {
+    setState({
+      keys: Array.from<unknown, DigiKey>({ length: TOTAL_KEYS_BY_DIFFICULTY[stateRef.current.difficulty] }, (_, i) => (
+        stateRef.current.keys[i] ?? {
           prongs: [],
           rotation: 0
         }
-      ))
-    })
-    setPuzzle(p => ({ layers: Array.from({ length: TOTAL_LAYERS_BY_DIFFICULTY[difficulty] }, (_, n) => (p.layers[n] ?? [])) }))
-  }, [difficulty]);
+      )),
+      puzzle: { layers: Array.from({ length: TOTAL_LAYERS_BY_DIFFICULTY[stateRef.current.difficulty] }, (_, n) => (stateRef.current.puzzle.layers[n] ?? [])) }
+    });
+  }, [state.difficulty]);
 
   useEffect(() => {
     // @ts-expect-error
     window.debug = () => {
       console.log('==== PUZZLE ====');
-      console.log(JSON.stringify(puzzle));
+      console.log(JSON.stringify(stateRef.current.puzzle));
       console.log('==== KEYS ====');
-      console.log(JSON.stringify(keys));
+      console.log(JSON.stringify(stateRef.current.keys));
     };
-  }, [puzzle, keys])
+  }, [])
 
   const onReset = useCallback(() => {
-    setKeys(Array.from<unknown, DigiKey>({ length: TOTAL_KEYS_BY_DIFFICULTY[difficulty] }, () => ({
-      prongs: [],
-      rotation: 0
-    })))
-    setPuzzle({ layers: Array.from({ length: TOTAL_LAYERS_BY_DIFFICULTY[difficulty] }, () => ([])) })
-    setSolved(false);
-    setActiveLayer('');
-  }, [difficulty]);
+    setState({
+      keys: Array.from<unknown, DigiKey>({ length: TOTAL_KEYS_BY_DIFFICULTY[stateRef.current.difficulty] }, () => ({
+        prongs: [],
+        rotation: 0
+      })),
+      puzzle: { layers: Array.from({ length: TOTAL_LAYERS_BY_DIFFICULTY[stateRef.current.difficulty] }, () => ([])) },
+      solved: false,
+      activeLayer: ''
+    })
+  }, []);
 
   const onSolve = useCallback(() => {
     try {
+      const { puzzle, keys } = stateRef.current;
       if (!puzzle.layers.every(l => l.length)) {
         throw new Error('Not all layers have been configured. Please ensure your puzzle is fully mapped!');
       }
@@ -108,12 +134,14 @@ export const AutoSolverProvider = ({ children }: PropsWithChildren) => {
       }
 
       try {
-        setKeys(solvePuzzle(puzzle, keys));
-        setActiveLayer('');
-        setSolved(true);
+        setState({
+          keys: solvePuzzle(puzzle, keys),
+          activeLayer: '',
+          solved: true
+        });
       } catch (e) {
         if (e instanceof NotSolveableError) {
-          setKeys(e.keys);
+          setState({ keys: e.keys });
         }
         throw new Error(
           'This puzzle is not solveable. ' +
@@ -124,40 +152,54 @@ export const AutoSolverProvider = ({ children }: PropsWithChildren) => {
       }
     } catch (e) {
       if (!(e instanceof Error)) return;
-      setError(e.message);
+
+      setState({ error: e.message });
     }
-  }, [puzzle, keys]);
+  }, []);
 
   useEffect(() => {
-    if (!error) {
+    if (!state.error) {
       return;
     }
-    const timeout = setTimeout(() => setError(''), 8000);
+    const timeout = setTimeout(() => setState({ error: undefined }), 8000);
     return () => clearTimeout(timeout);
-  }, [error]);
+  }, [state.error]);
 
-  const value = useMemo(() => ({
-    difficulty,
-    setDifficulty,
-    keys,
-    setKeys,
-    puzzle,
-    setPuzzle,
-    editKey,
-    setEditKey,
+  const actions = useMemo((): AutoSolverContextActions => {
+    const actionNames: (keyof AutoSolverContextActions)[] = [
+      'setActiveLayer',
+      'setDifficulty',
+      'setEditKey',
+      'setGapIllustrationMode',
+      'setGuides',
+      'setKeys',
+      'setMobileOffset',
+      'setPuzzle'
+    ];
+    return actionNames.reduce((actions, funcName) => {
+      const stateKey = funcName.charAt(3).toLowerCase() + funcName.slice(4) as keyof AutoSolverContextValue;
+      actions[funcName] = (value: any) => {
+        let result = value;
+        if (typeof value === 'function') {
+          result = value(stateRef.current[stateKey])
+          if (result === stateRef.current[stateKey]) {
+            return;
+          }
+        }
+        setState({ [stateKey]: result });
+      }
+      return actions;
+    }, {} as AutoSolverContextActions)
+  }, []);
+
+  const value = useMemo((): autoSolverContext => ({
+    ...state,
+    ...actions,
     onReset,
     onSolve,
-    solved,
-    error,
-    activeLayer,
-    setActiveLayer,
-    guides,
-    setGuides,
-    mobileOffset,
-    setMobileOffset,
-    gapIllustrationMode,
-    setGapIllustrationMode
-  }), [difficulty, setDifficulty, keys, setKeys, puzzle, setPuzzle, editKey, setEditKey, onReset, onSolve, solved, error, activeLayer, setActiveLayer, guides, setGuides, mobileOffset, setMobileOffset, gapIllustrationMode, setGapIllustrationMode])
+    state,
+    stateRef
+  }), [state, actions, onReset, onSolve]);
 
   return (
     <AutoSolverContext.Provider value={value}>
